9cd69490c9188d5605b5518de6b9705fa8cb0ab3
DenzelWong <veins2000wong@gmail.com>
2024-04-03 01:02:17 +0300

    lesson 5(1) HW

---
+++ dz5.pas
@@ -0,0 +1,12 @@
+﻿program qq;
+var d,t: integer;
+begin
+   writeln('Введите день недели: ');
+  read(d);
+   writeln('Введите время: ');
+  read(t);
+    if(d<=5) and (t>=18) then
+   write('Пришло время програмировать.')
+    else
+   write('Время не пришло!');
+  end.
