c4398009d90345738dd25eaa2cad2fa6ebd5e6d3
DenzelWong <veins2000wong@gmail.com>
2024-06-01 00:12:57 +0300

    урок 8 (массивы)

---
+++ урок8 (массивы).pas
@@ -0,0 +1,14 @@
+﻿program arrays;
+
+var
+  mas: array [0..7] of integer;
+  i: integer;
+  
+begin
+  
+  for i:=0 to 7 do
+    mas[i] := i;
+  writeln(mas);
+  
+
+end.
---
+++ урок8 (массивы)1.pas
@@ -0,0 +1,16 @@
+﻿program arrays;
+
+var
+  mas: array [0..7] of integer;
+  i: integer;
+  
+begin
+  
+  mas[0] := 5;
+  mas[1] := 10;
+  
+  for i:=2 to 7 do
+    mas[i] :=mas[i-2] + mas[i-1];
+  writeln(mas);
+  
+end.
