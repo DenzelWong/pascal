81956ab9eaefce3022955bb4b87d69706e5dc814
DenzelWong <veins2000wong@gmail.com>
2024-06-01 00:13:26 +0300

    урок 10 (калькулятор и мин и макс)

---
+++ урок 10 мин макс.pas
@@ -0,0 +1,17 @@
+﻿var a,min,max,i,n:integer;
+begin
+  write('Введите число: ');
+  read(n);
+  for i:=1 to n do
+  begin
+    read(a);
+    if i=1 then 
+    begin
+      max:=a;
+      min:=a;
+    end;
+     if max<=a then max:=a;
+     if min>=a then min:=a;
+  end;
+  writeln('min=',min,' max=',max);
+end.
---
+++ урок 10 простой калькулятор.pas
@@ -0,0 +1,15 @@
+﻿var a,b:real;
+var c:string;
+begin
+ readln(a);
+ readln(c); 
+ readln(b);
+ if c='-' then 
+   write('= ', a-b);
+ if c='+' then
+   write('= ', a+b);
+ if c='*' then
+   write('= ', a*b);
+ if c='/' then
+   write('= ', a/b);
+end.
