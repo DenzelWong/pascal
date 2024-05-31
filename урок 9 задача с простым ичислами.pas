82d17dcf782b58eabeb2897e8beeb63eb9312e57
DenzelWong <veins2000wong@gmail.com>
2024-06-01 00:13:43 +0300

    урок 9 (задачи)

---
+++ урок 9 задача с простым ичислами.pas
@@ -0,0 +1,23 @@
+﻿var n,c, i: integer;
+f:boolean;
+begin
+  write('Введите число: ');
+  read(n);
+  for i:=2 to n do
+  begin
+    f:=true;
+    for c:=2 to i-1 do
+    if i mod c = 0 then
+    begin
+      f:=false;
+      break;
+    end;
+    if f and (i = 2) then write(i)
+    else if f then write(',  ', i);
+  end;
+  if n<=1 then
+  writeln('Простые числа не найдены');
+end.
+ 
+   
+ 
---
+++ урок 9 задача фибоначчи.pas
@@ -0,0 +1,31 @@
+﻿var a, b, c, i, n: integer;
+begin
+  write('Ввeдите количество чисел Фибаначчи для вывода: ');
+  readln(n);
+  
+  if n <= 0 then
+    writeln('Количество должно быть больше 0.')
+  else if n = 1 then
+    write('Первoe числo последовательностu Фибоначчи: ')
+  else if (n >= 2) and (n <= 4) then 
+    write('Первые ', n, ' числа последовательностu Фибоначчи: ')
+  else 
+    write('Первые ', n, ' чисел последовательностu Фибоначчи: ');
+
+  a := 0;
+  b := 1;
+  
+  if n >= 1 then 
+    write(a);
+  
+  if n >= 2 then
+    write(' ', b);
+  
+  for i := 3 to n do
+  begin
+    c := a + b;
+    a := b;
+    b := c;
+    write(' ', c);
+  end;
+end.
