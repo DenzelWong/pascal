dff148deaa3c8f98d6af8487251b73f21fd07fc3
DenzelWong <veins2000wong@gmail.com>
2024-06-06 11:55:02 +0300

    Задачи по Операторам ветвления (условные операторы)

---
+++ Четное и нечетное число.pas
@@ -0,0 +1,14 @@
+﻿var a,b:integer;
+begin
+  writeln('Из двух чисел с разной четностью вывести на экран нечетное число.');
+  writeln('Введите одно четное и одно нечетное число: ');
+  read(a,b);
+  if a mod 2 <> 0 then
+    writeln(a, ' - Нечетное число')
+  else
+    writeln(b, ' - Нечетное число');
+  if b mod 2 = 0 then
+   writeln(b, ' - Четное число.')
+  else
+    writeln(a, ' - Четное число.');
+end.
---
+++ вывод макс числа.pas
@@ -0,0 +1,16 @@
+﻿var x1,x2,x3,max:integer;
+begin
+  writeln('Определить какое из трех введенных пользователем чисел максимальное.');
+  write('Вывидите три числа: ');
+  readln(x1,x2,x3);
+  if x1 > x2 then
+    max:=x1
+    else 
+  max:= x2;
+    if x2 > x3 then
+      max:=x2
+    else
+      max:=x3;
+    write('Максимальное число из них: ',max);
+  
+end.
